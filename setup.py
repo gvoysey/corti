ea565854cb45d8872c681e70e262c243be43dd33
Graham Voysey <gvoysey@bu.edu>
2016-03-28 21:22:42 -0400

    latest updates for setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup
+from distutils.core import setup
+
 
 # makes __version__ a local variable
 exec(open('verhulst_runner/_version.py').read())
@@ -8,13 +9,21 @@ setup(
     version=__version__,
     packages=['verhulst_runner',
               'verhulst_runner.analysis',
-              'verhulst_runner.utilities',
-              'verhulst_runner.utilities.stimulus_generator'],
+              ],
+    package_dir={'verhulst_runner': 'verhulst_runner'},
+    package_data={'verhulst_runner': ['resources/*']},
     scripts=['scripts/verhulst_model',
              'scripts/stimulus_generator'],
     url='https://github.com/gvoysey/thesis-code',
     license='',
     author='Graham Voysey',
     author_email='gvoysey@bu.edu',
-    description='A wrapper around https://github.com/AuditoryBiophysicsLab/verhulst-model-core'
+    description='A wrapper around https://github.com/AuditoryBiophysicsLab/verhulst-model-core',
+    install_requires=[
+        'docopt >= 0.6',
+        'matplotlib >= 1.5',
+        'numpy >= 1.10',
+        'progressbar2 >= 3.6',
+        'PyYAML > 3.10'
+    ]
 )
