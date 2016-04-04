ddc98ae8c4eeb4c63f30b64d3f690d459932de98
Graham Voysey <gvoysey@bu.edu>
2016-04-04 14:00:21 -0400

    removed blessings/Terminal support for windows compatibility.

--- verhulst_model_core/cochlear_model_old.py
+++ verhulst_model_core/cochlear_model_old.py
@@ -4,31 +4,31 @@ import platform
 
 import numpy as np
 import progressbar
-from blessings import Terminal
+#from blessings import Terminal
 from scipy import signal
 from scipy.integrate import ode
 
 from .core import *
 
-term = Terminal()
+#term = Terminal()
 
 
-class Writer(object):
-    """Create an object with a write method that writes to a
- specific place on the screen, defined at instantiation.
- This is the glue between blessings and progressbar.
- """
-
-    def __init__(self, location):
-        """
-     Input: location - tuple of ints (x, y), the position
-                       of the bar in the terminal
-     """
-        self.location = location
-
-    def write(self, string):
-        with term.location(*self.location):
-            print(string)
+# class Writer(object):
+#     """Create an object with a write method that writes to a
+#  specific place on the screen, defined at instantiation.
+#  This is the glue between blessings and progressbar.
+#  """
+#
+#     def __init__(self, location):
+#         """
+#      Input: location - tuple of ints (x, y), the position
+#                        of the bar in the terminal
+#      """
+#         self.location = location
+#
+#     def write(self, string):
+#         with term.location(*self.location):
+#             print(string)
 
 
 DOUBLE = ctypes.c_double
