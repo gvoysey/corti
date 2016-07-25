b443cae9894783f96f0e14892c48fc0b0123f262
Graham Voysey <gvoysey@bu.edu>
2016-07-24 22:48:04 -0400

    basic plotting now.

--- core/analysis/process_tone_in_noise.py
+++ core/analysis/process_tone_in_noise.py
@@ -1,11 +1,22 @@
 from logging import error
 
+import pandas as pd
 from os import path, walk
 from pypet import Trajectory
 
 
 # import matplotlib.pyplot as plt
 
+
+def plot_w5_vs_neuropathy(traj):
+    d = []
+    for run in traj.res.runs:
+        d.append({
+            'neuropathy': run.periphery.config.neuropathy,
+            'peakwave5' : max(run.brainstem.wave5.wave5),
+        })
+    df = pd.DataFrame(d)
+    df.plot()
 
 
 def make_plots(resultsPath):
@@ -15,9 +26,7 @@ def make_plots(resultsPath):
                 load_other_data=0, filename=resultsPath)
     traj.v_auto_load = True
 
-    for run in traj.res.runs:
-        w5 = run.brainstem.wave5['wave5']
-        peak = max(w5)
+    plot_w5_vs_neuropathy(traj)
 
     return 0
 
