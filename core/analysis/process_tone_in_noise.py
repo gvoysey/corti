53261033db457f6f6aacf2befb3e7103ac5e132a
Graham Voysey <gvoysey@bu.edu>
2016-07-28 18:32:47 -0400

    now plotting data frames directly to PDF.

--- core/analysis/process_tone_in_noise.py
+++ core/analysis/process_tone_in_noise.py
@@ -1,11 +1,19 @@
 from logging import error
 
 import matplotlib
-import pandas as pd
-from os import path, walk
-from pypet import Trajectory
 
 matplotlib.use('PDF')
+import pandas as pd
+from os import path
+from datetime import datetime
+import glob
+
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.ticker import FormatStrFormatter
+from pypet import Trajectory
+import matplotlib.pyplot as plt
+
+
 
 
 def plot_w5_vs_neuropathy(traj):
@@ -18,12 +26,37 @@ def plot_w5_vs_neuropathy(traj):
             'peaklatency': run.brainstem.wave5.wave5.argmax()
         })
     df = pd.DataFrame(d)
+
     df.plot()
 
-def plot_w5peaklatency_vs_snr(traj):
-    verhulst_healthy_no_weight_nc04 = [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                                                       not run.periphery.cf_weighting and
-                                                                       run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"]
+
+def plot_w5peaklatency_vs_snr(traj, pdf, pagenum):
+    fig = plt.figure(num=pagenum, figsize=(11, 8.5), dpi=400)
+    # containing many SNRs and many neuropathies
+    verhulst_no_weight_nc04 = [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
+                               not run.periphery.cf_weighting and
+                               run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"]
+
+    d = []
+    for run in verhulst_no_weight_nc04:
+        d.append({'neuropathy': run.periphery.config.neuropathy,
+                  'snr': run.periphery.snr.snr,
+                  'peaklatency': (run.brainstem.wave5.wave5.argmax() / 100e3) * 1e3})
+    df = pd.DataFrame(d)
+    ax = df.peaklatency.plot(xticks=df.index)
+    ax.set_xticklabels(df.neuropathy)
+    ax.set_xlabel("neuropathy type")
+    ax.set_ylabel("peak latency (ms)")
+    ax.set_title("foo")
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%2.2f'))
+    pdf.savefig(fig)
+    d = pdf.infodict()
+    d['Title'] = 'Auditory Periphery Model Output'
+    d['Author'] = "Graham Voysey <gvoysey@bu.edu>"
+    d['Keywords'] = 'ABR auditory model periphery'
+    d['CreationDate'] = datetime.today()
+    d['ModDate'] = datetime.today()
+
 
 
 
@@ -34,16 +67,15 @@ def make_plots(resultsPath):
     traj.f_load(load_parameters=2, load_derived_parameters=0, load_results=1,
                 load_other_data=0, filename=resultsPath)
     traj.v_auto_load = True
-
-    plot_w5_vs_neuropathy(traj)
+    with PdfPages(path.join(path.expanduser('~'), "pypet-output", 'summary.pdf')) as pdf:
+        plot_w5peaklatency_vs_snr(traj, pdf, 1)
 
     return 0
 
 
 if __name__ == "__main__":
     basepath = path.join(path.expanduser('~'), "pypet-output")
-    result = next(walk(basepath))
     try:
-        make_plots(path.join(basepath, result[2][0]))
+        make_plots(glob.glob(path.join(basepath, '*.hdf5'))[0])
     except IndexError:
         error("No simulations found to plot.")
