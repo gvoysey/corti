fa7c5240683f5f5fa81464819c7d1baafb8e14af
Graham Voysey <gvoysey@bu.edu>
2016-08-07 17:46:17 -0400

    adjusting scale bars.

--- core/analysis/process_tone_in_noise.py
+++ core/analysis/process_tone_in_noise.py
@@ -25,31 +25,42 @@ def print_relevant_properties(runs):
         weighting = "constant" if not r.periphery.cf_weighting else "logistic"
         neuropathy = r.periphery.config.neuropathy
         snr = r.periphery.snr.snr
-        print("{0} has periphery: {1}; brainstem {2}; with {3} weighting and neuropathy: {4}.  Stimulus snr was {5}".format(r.v_name, ptype, btype, weighting, neuropathy, snr))
+        print(
+                "{0} has periphery: {1}; brainstem {2}; with {3} weighting and neuropathy: {4}.  Stimulus snr was {5}".format(
+                        r.v_name, ptype, btype, weighting, neuropathy, snr))
 
 
 def plot_condition(runs_tuple, pdf, pagenum):
     run_name, runs = runs_tuple
 
     lines = {
-        'healthy': [r for r in runs if r.periphery.config.neuropathy == "none"],
-        'moderate': [r for r in runs if r.periphery.config.neuropathy == "moderate"],
-        'severe': [r for r in runs if r.periphery.config.neuropathy == "severe"],
+        'healthy'    : [r for r in runs if r.periphery.config.neuropathy == "none"],
+        'moderate'   : [r for r in runs if r.periphery.config.neuropathy == "moderate"],
+        'severe'     : [r for r in runs if r.periphery.config.neuropathy == "severe"],
         'ls_moderate': [r for r in runs if r.periphery.config.neuropathy == "ls-moderate"],
-        'ls_severe': [r for r in runs if r.periphery.config.neuropathy == "ls-severe"],
+        'ls_severe'  : [r for r in runs if r.periphery.config.neuropathy == "ls-severe"],
     }
     fig = plt.figure(num=pagenum, figsize=(11, 8.5), dpi=400)
     # plt.hold(True)
     legend_text = []
     max = -float("inf")
     min = float("inf")
+
+    for l in lines.items():
+        for run in l:
+            val = run.brainstem.wave5.wave5.argmax()
+            if val < min:
+                min = val
+            if val > max:
+                max = val
+
     for name, l in lines.items():
         print("\t neuropathy {0} had {1} runs".format(name, len(l)))
         d = []
         for run in l:
             d.append({
-                'snr': run.periphery.snr.snr,
-                'peaklatency': (run.brainstem.wave5.wave5.argmax() / 100e3) * 1e3,
+                'snr'           : run.periphery.snr.snr,
+                'peaklatency'   : (run.brainstem.wave5.wave5.argmax() / 100e3) * 1e3,
                 'wave1amplitude': run.brainstem.wave1.wave1.max()
             })
         legend_text.append(name)
@@ -63,6 +74,7 @@ def plot_condition(runs_tuple, pdf, pagenum):
         ax.set_xticklabels(df.snr)
         ax.set_xlabel("SNR")
         ax.set_ylabel("Peak Wave V Latency")
+        ax.set_ylim([min, max])
         ax.yaxis.set_major_formatter(FormatStrFormatter('%2.2f'))
     plt.legend(legend_text)
     plt.title(run_name)
@@ -72,71 +84,34 @@ def plot_condition(runs_tuple, pdf, pagenum):
 def plot_w5peaklatency_vs_snr(traj: Trajectory, pdf):
     # containing many SNRs and many neuropathies
     conditions = {
-        'verhulst_no_weight_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                    not run.periphery.cf_weighting and
-                                    run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'verhulst_no_weight_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
+        'verhulst_no_weight_nc04'  : [run for run in traj.res.runs if
+                                      run.periphery.modelType.casefold() == "verhulst" and
+                                      not run.periphery.cf_weighting and
+                                      run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
+        'verhulst_no_weight_carney': [run for run in traj.res.runs if
+                                      run.periphery.modelType.casefold() == "verhulst" and
                                       not run.periphery.cf_weighting and
                                       run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-        'verhulst_weighted_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                   run.periphery.cf_weighting and
-                                   run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'verhulst_weighted_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                     run.periphery.cf_weighting and
-                                     run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-        'zilany_no_weight_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                  not run.periphery.cf_weighting and
-                                  run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'zilany_no_weight_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                    not run.periphery.cf_weighting and
-                                    run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-        'zilany_weighted_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                 run.periphery.cf_weighting and
-                                 run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'zilany_weighted_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                   run.periphery.cf_weighting and
-                                   run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-    }
-
-    for i, c in enumerate(conditions.items()):
-        print("{0}, got {1} runs to plot".format(i, len(c[1])))
-        plot_condition(c, pdf, i)
-
-    d = pdf.infodict()
-    d['Title'] = 'Auditory Periphery Model Output'
-    d['Author'] = "Graham Voysey <gvoysey@bu.edu>"
-    d['Keywords'] = 'ABR auditory model periphery'
-    d['CreationDate'] = datetime.today()
-    d['ModDate'] = datetime.today()
-
-
-def plot_vs_snr(traj: Trajectory, pdf):
-    # containing many SNRs and many neuropathies
-    conditions = {
-        'verhulst_no_weight_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                    not run.periphery.cf_weighting and
-                                    run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'verhulst_no_weight_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
+        'verhulst_weighted_nc04'   : [run for run in traj.res.runs if
+                                      run.periphery.modelType.casefold() == "verhulst" and
+                                      run.periphery.cf_weighting and
+                                      run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
+        'verhulst_weighted_carney' : [run for run in traj.res.runs if
+                                      run.periphery.modelType.casefold() == "verhulst" and
+                                      run.periphery.cf_weighting and
+                                      run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
+        'zilany_no_weight_nc04'    : [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
+                                      not run.periphery.cf_weighting and
+                                      run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
+        'zilany_no_weight_carney'  : [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
                                       not run.periphery.cf_weighting and
                                       run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-        'verhulst_weighted_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                   run.periphery.cf_weighting and
-                                   run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'verhulst_weighted_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "verhulst" and
-                                     run.periphery.cf_weighting and
-                                     run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-        'zilany_no_weight_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                  not run.periphery.cf_weighting and
-                                  run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'zilany_no_weight_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                    not run.periphery.cf_weighting and
-                                    run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
-        'zilany_weighted_nc04': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                 run.periphery.cf_weighting and
-                                 run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
-        'zilany_weighted_carney': [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
-                                   run.periphery.cf_weighting and
-                                   run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
+        'zilany_weighted_nc04'     : [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
+                                      run.periphery.cf_weighting and
+                                      run.brainstem.modeltype.modeltype.casefold() == "nelsoncarney04"],
+        'zilany_weighted_carney'   : [run for run in traj.res.runs if run.periphery.modelType.casefold() == "zilany" and
+                                      run.periphery.cf_weighting and
+                                      run.brainstem.modeltype.modeltype.casefold() == "carney2015"],
     }
 
     for i, c in enumerate(conditions.items()):
@@ -153,7 +128,6 @@ def plot_vs_snr(traj: Trajectory, pdf):
 
 def make_plots(resultsPath):
     traj = Trajectory('tone-in-noise', add_time=False)
-
     traj.f_load(load_parameters=2, load_derived_parameters=0, load_results=1,
                 load_other_data=0, filename=resultsPath)
     traj.v_auto_load = True
